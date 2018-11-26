96a74bc3bc897c5902a98cf2936148ebb69fac00
Fredrik Kloster <fredrik_kloster@hotmail.com>
2018-11-26 14:42:11 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,79 @@ public class Main {
 
     public Main(){
 
-        int[] consts = {-2,0,2,-1,1,-3,3};
+        int[] xconsts1 = {-2,0,2,-1,1,+3};
+        int[] xconsts3 = {-3,-1,1,-2,0,2};
+        int[] xconsts7 = {-3,3,-2,0,2,-1,1,4};
+
 
         X = new ArrayList<>();
         Y = new ArrayList<>();
 
+
+
         //X
-        for(int i = 1; i != n; i++){
-            for(int j = 0; j < consts.length; j++){
-                int temp = (i * n/8) + consts[j];
-                X.add(temp);
+        boolean slayed1 = false;
+        boolean slayed2 = false;
+        boolean slayed3 = false;
+        int counter = 1;
+
+        while(true){
+            //First x
+            if(counter == CF(1,-2) && !slayed1){
+                for(int i  = 0 ; i < xconsts1.length - 1; i++){
+                    X.add(CF(1,xconsts1[i]));
+                }
+                counter = CF(1,xconsts1[xconsts1.length-1]);
+                slayed1 = true;
+                continue;
             }
+
+            //Second x
+            if(counter == CF(3,-3) && !slayed2){
+                for(int i  = 0 ; i < xconsts3.length - 1; i++){
+                    X.add(CF(3,xconsts3[i]));
+                }
+                counter = CF(3,xconsts3[xconsts3.length-1]);
+                slayed2 = true;
+                continue;
+            }
+
+            //Third x
+            if(counter == CF(7,-3) && !slayed3){
+                for(int i  = 0 ; i < xconsts7.length - 1; i++){
+                    X.add(CF(7,xconsts7[i]));
+                }
+                counter = CF(7,xconsts7[xconsts7.length-1]);
+                slayed3 = true;
+                continue;
+            }
+
+            X.add(counter);
+            counter++;
+            System.out.println(counter);
+            if(counter > n) break;
         }
 
+        counter = 1;
+        slayed1 = false;
 
+        while(true){
+            if(counter == CF(5,-2) && !slayed1){
+                for(int i  = 0 ; i < xconsts1.length - 1; i++){
+                    Y.add(CF(5,xconsts1[i]));
+                }
+                counter = CF(5,xconsts1[xconsts1.length-1]);
+                slayed1 = true;
+                continue;
+            }
+            Y.add(counter);
+            counter++;
+            if(counter > n) break;
+        }
+
+        System.out.println(X);
+        System.out.println(Y);
+        /*
         for(int i = 1; i != n; i++){
             for(int j = 0; j < consts.length; j++){
                 int temp = (i * n/5) + consts[j];
@@ -63,8 +122,12 @@ public class Main {
         List<Coordinate> commonC = getCommonCoordinates(Xcoordinates,Ycoordinates);
         System.out.println("Common coordz: " +  commonC.size() + " :: " + commonC);
 
+*/
 
+    }
 
+    public int CF(int row, int con){
+        return (row * n)/8 + con;
     }
 
 
